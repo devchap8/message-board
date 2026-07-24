fd66c2d41ef0650eb479469ca830013f411a3c06
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-07-24 15:28:19 -0400

    Start styling messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,12 +5,12 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date().toLocaleString('en-US')
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: new Date().toLocaleString('en-US')
   }
 ];
 
