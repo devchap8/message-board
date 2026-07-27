141935665ccaa9ef769c730a9fff36a0730665b0
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-07-27 08:49:16 -0400

    Make open msg button

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,12 +5,14 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date().toLocaleString('en-US')
+    added: new Date().toLocaleString('en-US'),
+    id: crypto.randomUUID()
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date().toLocaleString('en-US')
+    added: new Date().toLocaleString('en-US'),
+    id: crypto.randomUUID()
   }
 ];
 
@@ -27,9 +29,16 @@ indexRouter.post('/new', (req, res) => {
     messages.push({
         text: req.body['form-text'],
         user: req.body['form-name'], 
-        added: new Date().toLocaleString('en-US')
+        added: new Date().toLocaleString('en-US'),
+        id: crypto.randomUUID()
     })
     res.redirect('/');
+})
+
+indexRouter.get('/info/:id', (req, res, next) => {
+    const msg = messages.find(m => m.id === req.params.id);
+    if(msg) res.render('msg-info', {msg});
+    next();
 })
 
 module.exports = indexRouter;
